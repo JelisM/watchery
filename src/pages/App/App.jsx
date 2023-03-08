906c18fff70b1bad02e8493029321f0705f1004a
JelisM <jelismaiara@gmail.com>
2023-03-08 18:59:30 -0500

    message controller

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import ChildPage from '../ChildPage/ChildPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar'
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function App() {
   const [ user, setUser ] = useState(getUser())
